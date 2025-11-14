1f65e09a1e826203b6ec276bda8935f7bb39790d
mjwachira <mjwachira1@gmail.com>
2025-11-14 19:06:20 +0300

    vite app

--- frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// frontend/postcss.config.js
-export default {
-  plugins: {
-    // 🚨 CHANGE THIS LINE from 'tailwindcss' to the new package name
-    '@tailwindcss/postcss': {}, 
-    'autoprefixer': {},
-  },
-};
